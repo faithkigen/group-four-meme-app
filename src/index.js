abf57f69cf553d98414903c76f0d489a9cd2c78d
chris-wamae <chriswamae123@gmail.com>
2023-01-24 10:01:54 +0300

    Created MemesDataContext that fetches data for the whole application and stores it

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { NavigationProvider } from './data/NavigationContext';
+import { MemesDataProvider } from './data/MemesDataContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <MemesDataProvider>
     <NavigationProvider>
     <App />
     </NavigationProvider>
+    </MemesDataProvider>
   </React.StrictMode>
 );
 
