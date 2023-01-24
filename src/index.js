8e9b9c8e89129acdf7d8db4f39f6da1b255c88e6
chris-wamae <chriswamae123@gmail.com>
2023-01-24 08:29:17 +0300

    Built navigation prototype for the app using Context

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { NavigationProvider } from './data/NavigationContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <NavigationProvider>
     <App />
+    </NavigationProvider>
   </React.StrictMode>
 );
 
---
+++ src/views/MemesCollection.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MemesCollection(props) {
+    return (
+        <div>
+            <h2>All Memes</h2>
+        </div>
+    );
+}
+
+export default MemesCollection;
