e8691553b009f3199ab761819eedc2c1486f0d7c
kennedy <kenneyrit@gmail.com>
2023-01-25 09:53:41 +0300

    created and updated the login component

---
+++ src/Login.js
@@ -0,0 +1,20 @@
+import { useRef, useEffect, useState } from 'react';
+
+function Login() {
+    const userRef = useRef();
+    const errRef = useRef();
+
+    const [user, setUser] = useState('');
+    const [pwd, setPwd] = useState('');
+    const [errMsg, setErrMsg] = useState('');
+
+    useEffect(() => {
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Login
