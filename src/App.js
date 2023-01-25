95ca97a6ff7cf9e15aeb5fc1f2950f928ecdf27d
kennedy <kenneyrit@gmail.com>
2023-01-25 14:56:57 +0300

    Updated the route links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import Authenticator from './Authenticator'
+import Login from './Login';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  
+
   return (
+    // <Router>
+    //   <div>
+    //     <Routes>
+    //       <Route path="/" element={<Login />} />
+    //       <Route path="/auth" element={<Authenticator />} />
+    //     </Routes>
+    //     <footer/>
+    //   </div>
+    // </Router>
     <main className="App">
-      <Authenticator />
+      {/* <Authenticator /> */}
+      <Login />
      {/* <div>App</div> */}
     </main>
   );
