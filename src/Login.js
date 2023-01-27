ba9b2bdb5d8d478f89b8f49a83e4464acbec30a4
kennedy <kenneyrit@gmail.com>
2023-01-27 10:07:50 +0300

    updated the login form

--- src/Login.js
+++ src/Login.js
@@ -1,20 +1,85 @@
-import { useRef, useEffect, useState } from 'react';
+import { useRef, useEffect, useState, useContext } from "react";
 
 function Login() {
-    const userRef = useRef();
-    const errRef = useRef();
+  const userRef = useRef(); // sets the focus on the user input
+  const errRef = useRef(); // sets the focus on the error input
 
-    const [user, setUser] = useState('');
-    const [pwd, setPwd] = useState('');
-    const [errMsg, setErrMsg] = useState('');
+  const [user, setUser] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
 
-    useEffect(() => {
+  useEffect(() => {
+    // set the focus on the first input when the componentloads
+    userRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    setErrMsg("");
+  }, [user, pwd]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(user, pwd);
+    setUser("");
+    setPwd("");
+    setSuccess(true);
+  };
 
   return (
-    <div>
+    <>
+      {success ? (
+        <section className="landing-page">
+          <h1>Welcome !</h1>
+          <p>You have successfully logged in.</p>
+          <br />
+          <p>
+            <a href="">Logout</a>
+          </p>
+        </section>
+      ) : (
+        <section>
+          <p
+            ref={errRef}
+            className={errMsg ? "errmsg" : "offscreen"}
+            aria-live="asserive"
+          >
+            {errMsg}
+          </p>
+          <h1>Sign In</h1>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              id="username" // should match the htmlfor attribute
+              ref={userRef}
+              autoComplete="off"
+              onChange={(e) => setUser(e.target.value)}
+              value={user}
+              required
+            />
 
-    </div>
-  )
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password" // should match the htmlfor attribute
+              ref={userRef}
+              onChange={(e) => setPwd(e.target.value)}
+              value={pwd}
+              required
+            />
+            <button> Sign In</button>
+          </form>
+          Need an Account?
+          <br />
+          <span className="line"> 
+            {/* put router link here */}
+            <a href="#">Sign UP</a>
+          </span>
+        </section>
+      )}
+    </>
+  );
 }
 
-export default Login
+export default Login;
